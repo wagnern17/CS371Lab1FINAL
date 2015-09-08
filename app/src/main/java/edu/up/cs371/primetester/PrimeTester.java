8485b3800105f7140e63d72f98be1c4d269d1d90
wagnern17 <wagnern17@up.edu>
2015-09-08 16:16:12 -0700

    Final Commit Lab 1 CS371

---
+++ app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
@@ -0,0 +1,29 @@
+package edu.up.cs371.primetester;
+
+import android.util.*;
+/**
+ * Created by vegdahl on 7/17/2015.
+ *
+ * The purpose of this class is to define a static method, isPrime, that determines whether an
+ * integer is a prime number.
+ */
+public class PrimeTester {
+    /**
+     * Tells whether an integer is prime.
+     *
+     * @param k the number to test
+     * @return true iff n is prime
+     */
+    public static boolean isPrime(long k) {
+        if (k==0||k==1){
+            return false;
+        }
+        for (int i = 2;i<k;i++){
+            Log.i("isPrime", ""+k);
+            if (k%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
